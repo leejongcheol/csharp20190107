6b42e7317b10a663a5f06439eea9379e890be165
LeeJongCheol <leejongcheol2018@naver.com>
2019-01-08 16:08:27 +0900

    일차원 배열 실습

---
+++ ArrayTest/ArrayTest/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Linq;
+
+namespace ArrayTest
+{
+    class Program
+    {
+        static int[] iArr = new int[10];  //1차원 배열선언
+        static Random r = new Random();
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < iArr.Length; i++)
+            {
+                iArr[i] = r.Next(1, 10);
+            }
+            int sum = 0;
+            foreach (int i in iArr)
+            {
+                sum += i;
+                Console.Write(i + " ");
+            }
+            Console.WriteLine($"\n배열합은{sum}");
+            Console.WriteLine($"\n배열합은{iArr.Sum()}");
+        }
+    }
+}
