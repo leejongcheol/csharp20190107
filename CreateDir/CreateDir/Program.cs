f6ddb12880956e90b36022229dfecac8f2a090a5
LeeJongCheol <leejongcheol2018@naver.com>
2019-01-08 17:38:56 +0900

    디렉토리 생성

---
+++ CreateDir/CreateDir/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+namespace CreateDir
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (!Directory.Exists(@"C:\A"))
+            {
+                Directory.CreateDirectory(@"C:\A");
+            }
+        }
+    }
+}
