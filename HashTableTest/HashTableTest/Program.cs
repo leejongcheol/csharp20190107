165471519ab9b2e03d4e6bb7c4eafb379233d7e7
LeeJongCheol <leejongcheol2018@naver.com>
2019-01-09 10:21:59 +0900

    연산자 오보로딩 테스트

---
+++ HashTableTest/HashTableTest/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace HashTableTest{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable h = new Hashtable();
+            h.Add(1, "홍길동"); h.Add(2, "김길동"); h.Add(3, "정길동");
+            foreach(DictionaryEntry d in h)
+            {
+                Console.WriteLine($"키{d.Key}, 값:{d.Value}");
+            }
+
+            SortedList s = new SortedList(h);
+            foreach (DictionaryEntry d1 in s)
+            {
+                Console.WriteLine($"키{d1.Key}, 값:{d1.Value}");
+            }
+
+            Dictionary<int, string> d2 = new Dictionary<int, string>();
+            d2.Add(1, " 서울"); d2.Add(2, "부산"); d2.Add(3, "대전");
+            foreach (KeyValuePair<int, string> k in d2)
+            {
+                Console.WriteLine($"키{k.Key}, 값:{k.Value}");
+            }
+        }
+    }
+}
---
+++ OperatorTest/OperatorTest/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace OperatorTest{
+    class Add    {
+        public int Value { get; set; }
+        public static Add operator +(Add a1, Add a2)   {
+            Add a3 = new Add();
+            a3.Value = a1.Value + a2.Value;
+            return a3;
+        }
+    }
+    class Program    {
+        static void Main(string[] args)        {
+            Add a1 = new Add();  a1.Value = 1;
+            Add a2 = new Add();  a2.Value = 2;
+            Add a3 = a1 + a2;
+            Console.WriteLine(a3.Value);
+        }
+    }
+}
