2e55dee18f2ab1e76a3c5733fc292aa9b44d757e
LeeJongCheol <leejongcheol2018@naver.com>
2019-01-09 13:52:37 +0900

    델리게이트 실습

---
+++ DelegateTest/DelegateTest/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace DelegateTest{
+    class Program    {
+        //1 . 선언
+        //delegate int SumDelegate(int i, int j);
+        static void Main(string[] args)        {
+            //2. 생성
+            Program p = new Program();
+            Func<int, int, int> s =  p.Sum ;
+            //3. 실행
+            Console.WriteLine( s(1,2) );
+        }
+        int Sum(int i, int j)        {
+            return i + j;
+        }
+    }
+}
