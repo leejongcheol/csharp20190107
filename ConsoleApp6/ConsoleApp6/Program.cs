9ad7c2f0a1be532eb701665fea7f85456d5ddae2
LeeJongCheol <leejongcheol2018@naver.com>
2019-01-09 15:53:53 +0900

    Event Test

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace ConsoleApplication11{
+    class Program    {
+         static void Main(string[] args)   {
+            Func<int, int, int> c = MySum;   //이 델리게이트는 이름있는 외부 메소드를 참조
+            Console.WriteLine("1+2={0}", c(1, 2));
+
+            //델리게이트를 이용한 익명 메소드
+            Func<int, int, int> c1 = (i, j) => i + j;    //식형식람다, 값을 리턴함        
+            Console.WriteLine("3+4={0}", c1(3, 4));
+        }
+        static int MySum(int i, int j)         {
+            return i + j;
+        }
+    }
+}
---
+++ EventTest1/EventTest1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace ConsoleApplication1 {
+    class EventPublisher  {       
+        public event EventHandler MyEvent; //이벤트 정의
+        public void Do()  {
+            MyEvent?.Invoke(null, null);  //이벤트 발생, 호출
+        }
+    }
+    //구독자 클래스
+    class Subscriber    {
+        static void Main(string[] args)        {
+            EventPublisher p = new EventPublisher();           
+            p.MyEvent += new EventHandler(doAction);
+            p.Do();
+        }
+        static void doAction(object sender, EventArgs e)        {
+            Console.WriteLine("MyEvent 라는 이벤트 발생...");
+        }
+    }
+}
