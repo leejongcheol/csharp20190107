828860014f23651e1debbb42e5cd4d7c8a3275ec
LeeJongCheol <leejongcheol2018@naver.com>
2019-01-11 14:25:25 +0900

    Oracle Login Example

---
+++ Login/Login/Common_DB.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Data.OleDb;
+using System.Windows.Forms;
+namespace Login
+{
+    class Common_DB
+    {
+        // DataBase Connection
+        public static OleDbConnection DBConnection()
+        {
+            OleDbConnection Conn;
+            //아래는 오라클용 접속 문자열, data source 애는 tnsnames.ora 파일에
+            있는 Alias명을 넣으면 됩니다.
+ string ConStr = ("Provider=ORAOLEDB.ORACLE;data source=kaeri;User
+       ID = scott; Password = tiger");
+        Conn = new OleDbConnection(ConStr);
+            return Conn;
+        }
+        // DataSelect
+        public static OleDbDataReader DataSelect(string id, string sql,
+       OleDbConnection Conn)
+        {
+            try
+            {
+                OleDbCommand myCommand = new OleDbCommand(sql, Conn);
+                myCommand.Parameters.Add("@id", OleDbType.Char, 10).Value = id;
+                return myCommand.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                //Log File에 출력
+                Log.WriteLine("FrmLogin", ex.ToString());
+                MessageBox.Show(sql + "\n" + ex.Message, "DataSelect");
+                return null;
+            }
+            finally
+            {
+                //Log File에 출력
+                Log.WriteLine("FrmLogin", “data select ok!”);
+                return null;
+            }
+        }
+    }
+}
---
+++ Login/Login/Log.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+namespace Login
+{
+    class Log
+    {
+        // C드라이브에 dotnet폴더를만들자.
+        public static void WriteLine(string name, string e)
+        {
+            string filename = @"c:\dotnet\" + name;
+            string logtime = DateTime.Now.ToString();
+            FileStream aFile = new FileStream(filename, FileMode.OpenOrCreate);
+            StreamWriter aWriter = new StreamWriter(aFile, System.Text.Encoding.Default);
+            aWriter.WriteLine("[" + logtime + "] ");
+            aWriter.WriteLine(e);
+            aWriter.Flush();
+            aWriter.Close();
+        }
+    }
+}
