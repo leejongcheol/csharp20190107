0df016789200f06b0c4f57dcaca26d5f3db1983e
LeeJongCheol <leejongcheol2018@naver.com>
2019-01-11 10:58:43 +0900

    oracle test

---
+++ OracleTest/OracleTest/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Oracle.ManagedDataAccess.Client;
+namespace ConsoleApplication7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string str = "data source=topcredu; user id=scott; password=tiger";
+            string str = @"Data Source=(DESCRIPTION =
+ (ADDRESS = (PROTOCOL = TCP)(HOST = 192.168.0.27)(PORT = 15 21))
+ (CONNECT_DATA =
+ (SERVER = DEDICATED)
+(SERVICE_NAME = topcredu)
+ )
+ ) ;User Id=scott;Password=tiger"; 
+            OracleConnection Conn = new OracleConnection(str);
+            OracleCommand Comm = new OracleCommand();
+            Comm.Connection = Conn;
+            try
+            {
+                Conn.Open();
+                Comm.CommandText = "SELECT ENAME FROM EMP";
+                OracleDataReader reader = Comm.ExecuteReader();
+                while (reader.Read())
+                {
+                    Console.WriteLine(reader.GetString(reader.GetOrdinal("ENAME")));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            finally
+            {
+                Conn.Close();
+            }
+        }
+    }
+}
---
+++ TcpClientTest/TcpClientTest/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Net.Sockets;
+class TcpClientTest
+{
+    static void Main(string[] args)
+    {
+        TcpClient client = null;
+        try
+        {
+            client = new TcpClient();
+            client.Connect("192.168.0.35", 5001);
+            NetworkStream stream = client.GetStream();
+            StreamWriter writer = new StreamWriter(stream) { AutoFlush = true };
+            string dataToSend = Console.ReadLine();
+            while (true)
+            {
+                writer.WriteLine(dataToSend);
+                if (dataToSend.IndexOf("<EOF>") > -1) break;
+                dataToSend = Console.ReadLine();
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }
+        finally
+        {
+            client.Close();
+        }
+    }
+}
